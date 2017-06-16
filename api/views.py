591d6e647dfd68959c49530984e033ee5289ed96
Jonas Knorr <jonas>
2017-06-16 10:20:44 +0200

    fix hash creation bug

--- api/views.py
+++ api/views.py
@@ -25,7 +25,9 @@ class UpdateProductsView(APIView):
                 filtered_products = Product.objects.filter(domainManager=domainManager, serialNumber=serialNumber, objectClass=objectClass)
                 if not filtered_products:
                     new_id = int(hashlib.sha1("".join(map(str, [domainManager, objectClass, serialNumber])).encode()).hexdigest(), 16)
-                    short_id = int(new_id/math.pow(10, 32))
+                    #short_id = int(new_id/math.pow(10, 32))
+                    short_id = new_id % 2**32
+                    print(short_id)
                     new_product = Product(id=short_id, header=header, domainManager=domainManager, serialNumber=serialNumber, objectClass=objectClass, location=location )
                     new_product.save()
                 print(filtered_products)
